d2e0d17496e653fdf65827165178b41ebf310c0d
aguscarrera77 <aguscarrera77@gmail.com>
2025-09-12 12:31:34 -0300

    11/9

---
+++ practica.py
@@ -0,0 +1,16 @@
+print('hola mundo') 
+edad = 35
+experiencia= 4
+
+nombre='Agustin'
+profesion='Instructor'
+
+enviado=True
+noenviado=False
+
+print('Mi edad es',edad)
+print('Mi tiempo de laburo',experiencia)
+print("Mi nombre es ",nombre)
+print("Soy ",profesion)
+print('Enviado',enviado)
+print('No enviado',noenviado)
