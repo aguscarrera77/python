c66c2577608eb0202ab544953ce15075decfe44b
aguscarrera77 <aguscarrera77@gmail.com>
2025-09-18 16:39:56 -0300

    18/9

--- practica.py
+++ practica.py
@@ -1,16 +1,84 @@
-print('hola mundo') 
-edad = 35
-experiencia= 4
+#print('hola mundo') 
+#edad = 35
+#experiencia= 4
 
-nombre='Agustin'
-profesion='Instructor'
+#nombre='Agustin'
+#profesion='Instructor'
 
-enviado=True
-noenviado=False
+#enviado=True
+#noenviado=False
 
-print('Mi edad es',edad)
-print('Mi tiempo de laburo',experiencia)
-print("Mi nombre es ",nombre)
-print("Soy ",profesion)
-print('Enviado',enviado)
-print('No enviado',noenviado)
+#print('Mi edad es',edad)
+#print('Mi tiempo de laburo',experiencia)
+#print("Mi nombre es ",nombre)
+#print("Soy ",profesion)
+#print('Enviado',enviado)
+#print('No enviado',noenviado)
+
+#Concatenar y mostrar el texto
+
+nombre='Roberto'
+edad=30
+
+#Concatenar con comas.
+
+#print('Hola soy ',nombre,'como estas? y tengo',edad)
+
+#Concatenar con el signo +
+
+#print('Hola soy '+ nombre + ' como estas? y tengo '+ str(edad))
+
+#Operaciones Matematicas.
+
+#a=10 #int
+#b=3 #int
+#c=2.5 #float
+
+#print('Suma', a+b)
+#print('Resta', a-b)
+#print('Multiplacion', a*b)
+#print('Division',round ((a/b),2))#decimal
+#print('Division entera',a//b)
+#print('Resto',a%b)
+#print('Potencia',a**b )
+ 
+#Combinar operaciones.
+
+#total=(a-b)**b
+
+#print('Calculo total',total)
+
+#name='JA'*5
+
+#print(name)
+
+#Conversiones de valores.
+
+#age= input('Mi edad es')
+
+#print(type(age))
+
+#edades=int(input('Ingresa tu edad: '))
+#print('El ano que viene tendras ',edades + 1)
+
+#altura=float(input('Cuanto medis en metros:'))
+
+#print('tu altura en centimetros es',altura*100)
+
+numero=30
+
+#print("el numero es "+ str(numero))
+
+#Combinar input.
+
+numero1=int(input('Ingrese un numero '))
+numero2=int(input('Ingreso un numero '))
+
+print(numero1+numero2)
+
+#Area 
+base=float(input('Ingrese la base de su triangulo '))
+altura=float(input('Ingrese la altura de su triangulo'))
+area=float((base*altura)/2)
+
+print('El area de su triangulo es :',area)
