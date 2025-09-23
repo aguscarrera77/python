da62b97333fc4b66ae8ecf04e605832c84622764
aguscarrera77 <aguscarrera77@gmail.com>
2025-09-23 16:44:23 -0300

    23/9

--- practica.py
+++ practica.py
@@ -17,8 +17,8 @@
 
 #Concatenar y mostrar el texto
 
-nombre='Roberto'
-edad=30
+#nombre='Roberto'
+#edad=30
 
 #Concatenar con comas.
 
@@ -65,20 +65,83 @@ edad=30
 
 #print('tu altura en centimetros es',altura*100)
 
-numero=30
+#numero=30
 
 #print("el numero es "+ str(numero))
 
 #Combinar input.
 
-numero1=int(input('Ingrese un numero '))
-numero2=int(input('Ingreso un numero '))
+#numero1=int(input('Ingrese un numero '))
+#numero2=int(input('Ingreso un numero '))
 
-print(numero1+numero2)
+#print(numero1+numero2)
 
 #Area 
-base=float(input('Ingrese la base de su triangulo '))
-altura=float(input('Ingrese la altura de su triangulo'))
-area=float((base*altura)/2)
+#base=float(input('Ingrese la base de su triangulo '))
+#altura=float(input('Ingrese la altura de su triangulo'))
+#area=float((base*altura)/2)
 
-print('El area de su triangulo es :',area)
+#print('El area de su triangulo es :',area)
+
+
+#number=10
+
+#print(number>8)
+#numeros mayores a 0 y pares
+#num=50
+#num1=45
+#print(num>0 and  num%2==0)
+#print(num1>0 and num1%2==0)
+
+#Usuario sean mayor o igual a 18 y que sea usuario.
+
+#usuario=True
+#edad=17
+#mensaje={True:'Usted es usuario y tiene mas de 18',False:'Usted no cumple las condiciones'}
+#print(mensaje[usuario and edad>18])
+
+#Comparar 3 numeros y determinar que al menos 2 sean iguales
+
+#a=3
+#b=5
+##c=3
+
+#print((a==b)or(a==c)or(b==c))
+
+#numero=int(input('Ingrese un numero'))
+
+#print(numero>100)
+
+#x=int(input('Ingreso un numero mayor a 0'))
+#y=int(input('Ingrese numero menor a 100'))
+
+#print(x>=y)
+
+#verificar un password
+
+#clave=input('Su password es:')
+
+#print(clave=='agus') 
+
+#Transformar letras.
+
+#letra=input('Ingrese vocales').lower()
+
+#print(letra in 'aeiou')
+
+#dos condiciones para poder anotarse a un curso. Si es o no estudiante y si es mayor a 21.
+
+#estudiante= input('Sos estudiante?(s/n)').lower()
+#edad=int(input('Que edad tenes?'))
+
+#print(estudiante== 's' and edad>=21)
+
+#Primer if genera una condicion esa condicion es true se imprime el codigo si es false no se imprime el codigo genero una respuesta con el metodo else para salvar el false.
+
+age=20
+
+if age>20:
+    print('Es mayor a 18')
+else:
+     print('Es menor a 18')      
+    
